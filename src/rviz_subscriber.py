405cf56a9ab6024b2d84742d828fe4241a34308e
cs2r <centerofroboticsg108@gmail.com>
2018-04-19 09:22:19 +0300

    gripper

--- src/rviz_subscriber.py
+++ src/rviz_subscriber.py
@@ -9,16 +9,15 @@ class Gripper():
         joint_cmd = JointState()
         now = rospy.Time.now()
         joint_cmd.header.stamp = now
-        joint_cmd.name = joint
+        joint_cmd.name = [joint]
         joint_cmd.position = [data.data]
-        print joint_cmd
         self.pub.publish(joint_cmd)
 
     def __init__(self):
         self.cmd = {}
         self.joints = ["shoulder", "elbow", "wrist_pitch", "wrist_rol", "gripper"]
         rospy.init_node('Robot_controller')
-        self.pub = rospy.Publisher("/robot_joint_command", JointState, queue_size=10)
+        self.pub = rospy.Publisher("robot_joint_command", JointState, queue_size=10)
         for joint in self.joints:
             rospy.Subscriber(joint, Float64, self.callback, callback_args=joint)
         rospy.spin()
